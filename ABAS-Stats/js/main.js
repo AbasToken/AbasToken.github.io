f191629a40b826c81478ececf5160499eb8ec28a
ABAS Token <120234697+AbasToken@users.noreply.github.com>
2024-12-08 11:41:05 -0600

    Update main.js

--- ABAS-Stats/js/main.js
+++ ABAS-Stats/js/main.js
@@ -50,7 +50,7 @@ const _MINIMUM_TARGET_BN = new Eth.BN(_MINIMUM_TARGET);
 const _IDEAL_BLOCK_TIME_SECONDS = 60 * 12  //_ETH_BLOCKS_PER_REWARD * _SECONDS_PER_ETH_BLOCK;
 
 /* TODO: figure out why it doesn't work w metamask */
-var eth = new Eth(new Eth.HttpProvider("https://arbitrum-mainnet.infura.io/v3/5127ae1e5ebc4c999274f109eca22dd4"));
+var eth = new Eth(new Eth.HttpProvider("https://arbitrum-mainnet.infura.io/v3/b8af241874c24ec78721b46e19c26c8a"));
 // if (typeof window.web3 !== 'undefined' && typeof window.web3.currentProvider !== 'undefined') {
 //   var eth = new Eth(window.web3.currentProvider);
 // } else {
